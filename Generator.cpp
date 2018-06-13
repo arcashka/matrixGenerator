a25822b72b0dda40f3c791767824c09fb73f3a5c
arcashka <pewpewstol@gmail.com>
2018-06-14 05:21:26 +0700

    Fix error making ig

--- Generator.cpp
+++ Generator.cpp
@@ -9,6 +9,7 @@
 
 std::shared_ptr<Matrix> Generator::Generate(int size, int sparseness, std::vector<double> & x)
 {
+	std::cout << "pew" << std::endl;
 	std::vector<int>    ig;
 	std::vector<int>    jg;
 	std::vector<double> ggl;
@@ -24,31 +25,28 @@ std::shared_ptr<Matrix> Generator::Generate(int size, int sparseness, std::vecto
 	std::srand(std::time(nullptr));
 
 	int gglNum = 0;
+	int count  = 0;
 	double gglMult = 10.0;
 	double diMult  = 1000.0;
 
 	for(int i = 0; i < size; i++)
 	{
-		int count = 0;
-		ig.push_back(gglNum);
+		ig.push_back(count);
 		for(int j = 0; j < i; j++, gglNum++)
 		{
-			std::cout << i << " " << gglNum << std::endl;
 			if(gglNum % sparseness == 0)
 			{
-				std::cout << "in" << std::endl;
 				ggl.push_back((double(std::rand()) / double(RAND_MAX)) * gglMult);
 				jg.push_back(j);
 				count++;
 			}
 		}
 	}
-	std::cout << "pew" << std::endl;
+	ig.push_back(count);
 
 	for(int i = 0; i < size; i++)
 		di.push_back((double(std::rand()) / double(RAND_MAX)) * diMult);
 
-	std::cout << "pew";
 	std::vector<double> b;
 	auto matrix = std::make_shared<Matrix>(
 		Matrix {
@@ -59,28 +57,9 @@ std::shared_ptr<Matrix> Generator::Generate(int size, int sparseness, std::vecto
 			b
 		}
 	);
+
 	b = matrix * x;
 	matrix->b = std::move(b);
-
-	std::cout << "di" << std::endl;
-	for(auto a : matrix->di)
-		std::cout << a << " ";
-	std::cout << std::endl;
-
-	std::cout << "ggl" << std::endl;
-	for(auto a : matrix->ggl)
-		std::cout << a << " ";
-	std::cout << std::endl;
-
-	std::cout << "ig" << std::endl;
-	for(auto a : matrix->ig)
-		std::cout << a << " ";
-	std::cout << std::endl;
-
-	std::cout << "jg" << std::endl;
-	for(auto a : matrix->jg)
-		std::cout << a << " ";
-	std::cout << std::endl;
 
 	return matrix;
 };
